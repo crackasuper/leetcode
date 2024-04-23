4fa83b67c666ddafb1fa0ca3da74154d784d159f
Sadem Hussen <154666429+crackasuper@users.noreply.github.com>
2024-04-23 16:29:08 +0300

    Time: 734 ms (82.47%), Space: 24 MB (55.84%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
@@ -1,11 +1,11 @@
 class Solution:
   def reductionOperations(self, nums: List[int]) -> int:
-    ans = 0
+        ans = 0
 
-    nums.sort()
+        nums.sort()
 
-    for i in range(len(nums) - 1, 0, -1):
-      if nums[i] != nums[i - 1]:
-        ans += len(nums) - i
+        for i in range(len(nums) - 1, 0, -1):
+          if nums[i] != nums[i - 1]:
+            ans += len(nums) - i
 
-    return ans
+        return ans
