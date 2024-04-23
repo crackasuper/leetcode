9c3aa65c1dee2a6a6e45696a0e0de9c6a23d57f7
Sadem Hussen <154666429+crackasuper@users.noreply.github.com>
2024-04-23 21:26:16 +0300

    Time: 476 ms (92.53%), Space: 29.8 MB (7.64%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -3,3 +3,13 @@ class Solution:
     count = collections.Counter(nums)
     return sum(min(count[num], count[k - num])
                for num in count) // 2
+    # count = 0
+    # left = 0
+    # right = len(nums) - 1
+    # while right >= left:
+    #     if nums[left] + nums[right] == k:
+    #         count += 1
+    #         nums.remove(nums[left])
+    #         nums.remove(nums[right])
+    #     right -= 1
+    # return count
