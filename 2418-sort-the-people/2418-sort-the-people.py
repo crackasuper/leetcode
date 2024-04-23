680fbaedafd33e5744a3b88a899ee4ee394e94fd
Sadem Hussen <154666429+crackasuper@users.noreply.github.com>
2024-04-23 13:21:12 +0300

    Time: 400 ms (12.70%), Space: 20.3 MB (8.96%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,6 +1,16 @@
+##insertion sort
+#counting sort
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        data = list(range(len(heights)))
-        data.sort(key = lambda i: -heights[i])
-        return [names[i] for i in data]
-        
+        maxHeights = max(heights)
+        count = [0] * (maxHeights + 1)
+        for i in heights:
+            count[i] += 1
+        for i in range(1, maxHeights + 1):
+            count[i] += count[i -1]
+        res_name = [''] * len(names)
+        for i in range(len(names) - 1, -1, -1):
+           res_name[count[heights[i]] - 1] = names[i]
+           count[heights[i]] -= 1
+    
+        return reversed(res_name)
