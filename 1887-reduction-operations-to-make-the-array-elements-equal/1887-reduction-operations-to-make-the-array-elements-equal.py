ba7d53dce0df621418ed5f731a2edf0a7091add6
Sadem Hussen <154666429+crackasuper@users.noreply.github.com>
2026-03-06 22:20:44 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
@@ -1,11 +1,19 @@
-class Solution:
-  def reductionOperations(self, nums: List[int]) -> int:
+class Solution(object):
+    def reductionOperations(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
         ans = 0
 
         nums.sort()
 
         for i in range(len(nums) - 1, 0, -1):
-          if nums[i] != nums[i - 1]:
-            ans += len(nums) - i
+            if nums[i] != nums[i - 1]:
+                ans += len(nums) - i
 
         return ans
+
+
+        
