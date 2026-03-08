be5291b97fb097c3000ed12f2ee24e0d71ebded5
Sadem Hussen <154666429+crackasuper@users.noreply.github.com>
2026-03-08 21:08:32 +0300

    Time: 0 ms (100%), Space: 12.4 MB (86.6%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,14 +1,14 @@
-class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
         """
-        Do not return anything, modify nums1 in-place instead.
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-       
         while len(nums1) > m:
             nums1.remove(nums1[-1])
         nums1 += nums2
         
         nums1.sort()
-
-        
-        
