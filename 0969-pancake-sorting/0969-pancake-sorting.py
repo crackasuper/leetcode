214f9e12f5a2b60667e19d2963ff6c9c1942cca0
Sadem Hussen <154666429+crackasuper@users.noreply.github.com>
2026-03-18 10:02:20 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0969-pancake-sorting/0969-pancake-sorting.py
+++ 0969-pancake-sorting/0969-pancake-sorting.py
@@ -1,12 +1,18 @@
-class Solution:
-  def pancakeSort(self, arr: List[int]) -> List[int]:
-    ans = []
+class Solution(object):
+    def pancakeSort(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        ans = []
 
-    for i in range(len(arr), 0, -1):
-      index = arr.index(i)
-      arr[:index + 1] = arr[:index + 1][::-1]
-      arr[:i] = arr[:i][::-1]
-      ans.append(index + 1)
-      ans.append(i)
+        for i in range(len(arr), 0, -1):
+            index = arr.index(i)
+            arr[:index + 1] = arr[:index + 1][::-1]
+            arr[:i] = arr[:i][::-1]
+            ans.append(index + 1)
+            ans.append(i)
 
-    return ans
+        return ans
+
+            
