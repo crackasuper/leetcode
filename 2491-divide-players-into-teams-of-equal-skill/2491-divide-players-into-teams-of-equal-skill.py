d22342b162274b8eafd9ef9eb02fe02483f91c13
Sadem Hussen <154666429+crackasuper@users.noreply.github.com>
2026-03-18 16:00:25 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
@@ -1,13 +1,26 @@
-class Solution:
-    def dividePlayers(self, skill: List[int]) -> int:
+class Solution(object):
+    def dividePlayers(self, skill):
+        """
+        :type skill: List[int]
+        :rtype: int
+        """
+
         skill.sort()
-        cond = skill[0] + skill[-1]
-        i = 0
-        j = len(skill) - 1
+        
+        max_value = skill[0] + skill[-1]
+
+        left, right  =  0, len(skill) - 1
+
         ans = 0
-        while j > i:
-            if skill[i] + skill[j] != cond:
+
+        while right > left:
+            if skill[left] + skill[right] != max_value:
                 return -1
-            ans += skill[i] * skill[j]
-            i,j = i +1, j-1
+            ans += (skill[left] * skill[right])
+
+            left += 1
+            right -= 1
+
+
         return ans
+        
