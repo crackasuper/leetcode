f7183cbe9deae078e39dabf72bf83eb5037ef8a9
Sadem Hussen <154666429+crackasuper@users.noreply.github.com>
2026-03-20 17:53:15 +0300

    Time: 19 ms (46.37%), Space: 12.6 MB (85.24%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,12 +1,28 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        res = []
-        for i in s:
-            if i.isalnum():
-                res.append(i.lower())
-        ans = "".join(res)
-        return ans == ans[::-1]
-        
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
 
-      
-       
+        left_index = 0
+        right_index = len(s) - 1
+
+    
+        while left_index < right_index:
+            # Skip non-alphanumeric character on the left
+            if not s[left_index].isalnum():
+                left_index += 1
+     
+            elif not s[right_index].isalnum():
+                right_index -= 1
+        
+            elif s[left_index].lower() != s[right_index].lower():
+                return False
+          
+            else:
+                left_index += 1
+                right_index -= 1
+
+        return True
+        
