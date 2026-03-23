361da7e6c0461e23fe63a458442327b1b4f3bbfd
Sadem Hussen <154666429+crackasuper@users.noreply.github.com>
2026-03-24 00:18:00 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        # Choose nums[i] as the first number in the triplet, then search the
+        # remaining numbers in [i + 1, n - 1].
+        l = i + 1
+        r = len(nums) - 1
+        while l < r:
+            summ = nums[i] + nums[l] + nums[r]
+            if summ == target:
+            return summ
+            if abs(summ - target) < abs(ans - target):
+            ans = summ
+            if summ < target:
+            l += 1
+            else:
+            r -= 1
+
+        return ans
+
+
+
+        
