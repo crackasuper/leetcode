d82dcad1f931150a7213dcb8127210cd2128c8fc
Sadem Hussen <154666429+crackasuper@users.noreply.github.com>
2026-03-24 07:34:30 +0300

    Time: 127 ms (12.19%), Space: 13.4 MB (65.96%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,18 +1,33 @@
-class Solution:
-  def findAnagrams(self, s: str, p: str) -> List[int]:
-    ans = []
-    count = collections.Counter(p)
-    required = len(p)
+class Solution(object):
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
 
-    for r, c in enumerate(s):
-      count[c] -= 1
-      if count[c] >= 0:
-        required -= 1
-      if r >= len(p):
-        count[s[r - len(p)]] += 1
-        if count[s[r - len(p)]] > 0:
-          required += 1
-      if required == 0:
-        ans.append(r - len(p) + 1)
+        length_s, length_p = len(s), len(p)
 
-    return ans
+        window_counter =  Counter(s[:length_p - 1])
+        p_pattern = Counter(p)
+
+        result = []
+
+
+        for  i in range(length_p - 1, length_s):
+            window_counter[s[i]] += 1
+
+            if p_pattern == window_counter:
+                result.append(i - length_p + 1)
+
+            left_most = s[i - length_p + 1]#removing left most from the string
+            window_counter[left_most] -= 1 # remove first value to slide window
+
+            if window_counter[left_most] == 0:
+                del window_counter[left_most]
+
+        return result
+
+
+
+        
